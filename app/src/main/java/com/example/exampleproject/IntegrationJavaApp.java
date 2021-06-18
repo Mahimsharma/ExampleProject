e363d1b02d149cfd80006732b65d4eef715696fe
Mahimsharma <mahim136@gmail.com>
2021-06-18 09:11:48 +0530

    integrated Banuba sdk successfully

---
+++ app/src/main/java/com/example/exampleproject/IntegrationJavaApp.java
@@ -0,0 +1,36 @@
+package com.example.exampleproject;
+
+import android.app.Application;
+
+import com.banuba.sdk.audiobrowser.di.AudioBrowserKoinModule;
+import com.example.exampleproject.videoeditor.di.VideoEditorKoin;
+
+import com.banuba.sdk.arcloud.di.ArCloudKoinModule;
+import com.banuba.sdk.effectplayer.adapter.BanubaEffectPlayerKoinModule;
+import com.banuba.sdk.gallery.di.GalleryKoinModule;
+import com.banuba.sdk.token.storage.di.TokenStorageKoinModule;
+
+import org.koin.core.context.GlobalContext;
+
+import static org.koin.android.ext.koin.KoinExtKt.androidContext;
+import static org.koin.core.context.GlobalContextExtKt.startKoin;
+
+public class IntegrationJavaApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        startKoin(GlobalContext.INSTANCE, koinApplication -> {
+            androidContext(koinApplication, this);
+            koinApplication.modules(
+                    new AudioBrowserKoinModule().getModule(), // use this module only if you bought it
+                    new ArCloudKoinModule().getModule(),
+                    new TokenStorageKoinModule().getModule(),
+                    new VideoEditorKoin().getModule(),
+                    new GalleryKoinModule().getModule(),
+                    new BanubaEffectPlayerKoinModule().getModule()
+            );
+            return null;
+        });
+    }
+}
--- app/src/main/java/com/example/exampleproject/MainActivity.java
+++ app/src/main/java/com/example/exampleproject/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.exampleproject;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.banuba.example.integrationapp.videoeditor.IntegrationAppExportVideoContract;
+import com.banuba.sdk.ve.flow.VideoCreationActivity;
+import com.banuba.sdk.veui.ui.ExportResult;
 
 public class MainActivity extends AppCompatActivity {
-
+    public ActivityResultLauncher createVideoRequest;
+    Button button;
+    static IntegrationAppExportVideoContract integrationAppExportVideoContract = new IntegrationAppExportVideoContract();
+    ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createVideoRequest =  this.registerForActivityResult(integrationAppExportVideoContract,this::onResult);
+        button = findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onButtonClick();
+            }
+        });
+    }
+
+    public void onButtonClick(){
+        Log.d("CLICK", "onButtonClick: ");
+        Intent videoCreationIntent =  VideoCreationActivity.buildIntent(this,null,null);
+        createVideoRequest.launch(videoCreationIntent);
+    }
+    public void onResult(ExportResult exportResult){
+        Log.d("video exported", "onResult: "+ exportResult.toString());
     }
 }
